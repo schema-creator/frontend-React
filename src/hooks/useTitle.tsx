c615606e3a8de2714f41c709d2e18bbfa9566d0b
taku10101 <taku1011ht@gmail.com>
2024-02-01 14:29:22 +0900

    fix any

--- src/hooks/useTitle.tsx
+++ src/hooks/useTitle.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 const useTitle = () => {
   const [open, setOpen] = useState(false);
-  const [titleState, setTitle] = useState();
+  const [titleState, setTitle] = useState<string>("");
   const handleOpen = () => {
     setOpen(!open);
   };
-  const handleTitle = (e: any) => {
-    setTitle(e.target.value);
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value;
+    setTitle(title);
   };
   return { open, setOpen, titleState, setTitle, handleOpen, handleTitle };
 };
