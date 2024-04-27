04574e51f8ae3c77e1d8488c26762a03abeb9b5c
taku10101 <taku1011ht@gmail.com>
2024-04-27 17:03:47 +0900

    yamada ui add

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import { DialogDemo } from "@/views/Home";
 import "./index.css";
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<DialogDemo />} />
-        {/* <Route path='/projects' element={<Projects />} />
-        <Route path='/projects/:id' element={<ProjectById />} /> */}
+        <Route path='/' element={<p>Home</p>} />
         <Route path='*' element={<p>404</p>} />
       </Routes>
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
+import { defaultConfig, getColorModeScript } from "@yamada-ui/react";
+import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+function injectScript(): Plugin {
+  return {
+    name: "vite-plugin-inject-scripts",
+    transformIndexHtml(html) {
+      const content = getColorModeScript({
+        initialColorMode: defaultConfig.initialColorMode,
+      });
+
+      return html.replace("<body>", `<body><script>${content}</script>`);
     },
-  },
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), injectScript()],
 });
